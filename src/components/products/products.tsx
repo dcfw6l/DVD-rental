9aa74a33a265bac756a7f3a6b55b1fa252dc0fa7
szkristof <54985490+dcfw6l@users.noreply.github.com>
2020-11-17 16:07:49 +0100

    Add files via upload

---
+++ src/components/products/products.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./products.css"
+
+export class Home extends React.Component<{},{}> {
+    
+
+
+render(){
+    return <div className="homeContainer">
+        <h1>Üdvözöljük a DVD Kölcsönző weboldalán!</h1>
+
+        <h2>Felkapott termékek:</h2>
+        
+        </div>
+        
+}
+
+}
