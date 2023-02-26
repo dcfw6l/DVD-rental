df8a4d2163f6c1a62f9b4f4960f1e9533fb90a11
szkristof <szaszkristooon99@gmail.com>
2023-02-26 18:01:31 +0100

    metin component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/home/home';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { Products } from './components/products/products';
 import { Contact } from './components/contact/contact';
+import { Metin } from './components/metin/metin';
 
 
 
@@ -14,15 +15,17 @@ function App() {
       <Header></Header>
     <div className = "menuContainer">
       <Router>
-      <Link className="menuItems" to="/"> Főoldal</Link>
-      <Link className="menuItems" to="/Products"> Termékek</Link>
-      <Link className="menuItems" to="/Search"> Keresés</Link>
-      <Link className="menuItems" to="/Rental"> Kölcsönzés</Link>
-      <Link className="menuItems" to="/Contact"> Kapcsolat</Link>
+      <Link className="menuItems" to="/">Főoldal</Link>
+      <Link className="menuItems" to="/Products">Termékek</Link>
+      <Link className="menuItems" to="/Search">Keresés</Link>
+      <Link className="menuItems" to="/Rental">Kölcsönzés</Link>
+      <Link className="menuItems" to="/Contact">Kapcsolat</Link>
+      <Link className="menuItems" to="/Metin">Metin</Link> 
       
       <Route exact path="/" component={() => <Home />} />
       <Route exact path="/Products" component={() => <Products />} />
       <Route exact path="/Contact" component={() => <Contact />} />
+      <Route exact path="/Metin" component={() => <Metin />} />
         
       </Router>
     </div></div>
---
+++ frontend/src/components/metin/metin.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export class Metin extends React.Component {
+
+    render() {
+        return <div>
+            METIN 2 FOREVER            
+        </div>
+    }
+}
