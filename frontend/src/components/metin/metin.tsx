a0b2d8d92a7ed02f24db75108d9e49a4433e82ad
szkristof <szaszkristooon99@gmail.com>
2023-02-26 18:26:54 +0100

    s

--- frontend/src/components/metin/metin.tsx
+++ frontend/src/components/metin/metin.tsx
@@ -2,9 +2,38 @@ import React from "react"
 
 export class Metin extends React.Component {
 
+
+    private loszar: number = 2;
+    private ahahha;
+
+
     render() {
         return <div>
-            METIN 2 FOREVER            
+            METIN 2 FOREVER        
+            {this.loszar}    
+            <div>
+                <table>
+                    <thead style={{color: "red"}}>
+                        <tr>
+                            <th>Hehe</th>
+                            <th>Hehe</th>
+                            <th>Hehe</th>
+                        </tr>                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Haha</td>
+                            <td>Haha</td>
+                            <td>Haha</td>
+                        </tr>
+                        <tr>
+                            <td>Haha</td>
+                            <td>Haha</td>
+                            <td>Haha</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     }
 }
